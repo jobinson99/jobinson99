f1fa0efad07dbb2b01fc25508be2a2feeabb5332
黑传说(Atlas Jobinson) <jobinson99@gmail.com>
2014-05-17 12:26:10 +0800

    迁移完毕

--- themes/atlas/javascript/outliner.js
+++ themes/atlas/javascript/outliner.js
@@ -1,70 +1,3 @@
-/**
- * created by orz@mad4a.me with pirated webstorm
- * 根据 h2 来获得文内大纲
- */
-
-$(function() {
-    var dict = {};
-    $('h2').each(function (idx) {
-        var title = $(this).text();
-        var id = 'head_' + idx;
-        dict[title] = id;
-
-/*        $(this).append('<a name="' + id + '"></a>'); */
-$(this).html('<a name="' + id + '"></a>' + $(this).html()  );
-    });
-
-    var outline_ul = $('<ul></ul>');
-    $.each(dict, function (idx, val) {
-        outline_ul.append($('<li></li>')
-                             .html('<a href="#' + val + '">' + idx + '</a>'));
-    });
-    $('article').append($('<nav class="outline"></nav>')
-                      .css('top', $('.right-middle-hook').position().top - 1)
-                         .html(outline_ul));
-
-    /**
-     * 位置设定
-     * |<------------------------------w------------------------------>|
-     * |       -----------     -----------------     -----------       |
-     * |<--l-->|   nav   |<-d->|               |<-d->| outline |<--x-->|
-     * |       |<---n--->|     |<------c------>|     |<---a--->|       |
-     * |       -----------     |               |     -----------       |
-     * |<----------m---------->|               |                       |
-     * |                       -----------------                       |
-     * -----------------------------------------------------------------
-     * (w - c) / 2 = d + a + x
-     *   => x = (w - c) / 2 - (a + d), where
-     *     w = $(window).width(),
-     *     c = $('#container').width(),
-     *     a = $('h2outline').width(),
-     *
-     * m = l + n + d
-     *   => d = m - (l + n), where
-     *     m = $('#container').position().left,
-     *     l = $('#real_nav').position().left,
-     *     n = $('#real_nav').width()
-     */
-    // var main = $('.post'),
-    //     outline = $('.outline'),
-    //     nav_hook  = $('.right-middle-hook');
-
-    // var m = main.position().left,
-    //     l = nav_hook.position().left,
-    //     n = nav_hook.width(),
-    //     d = m - (l + n) + 184.8;
-
-    // $(window).resize(function () {
-    //     var w = $(window).width(),
-    //         c = main.width(),
-    //         a = outline.width();
-    //     outline.css('right',
-    //                   (w - c) / 2 - (a + d));
-    // });
-
-    // $(window).resize();
-
-});
 
     $(document).ready(function(){
         $('.site-action').hide();
@@ -101,6 +34,8 @@ $(this).html('<a name="' + id + '"></a>' + $(this).html()  );
         
         //实现左侧底部的按钮浮动
         //设定图标的动画：鼠标移动到上面之后的模糊放大旋转，点击之后四散？
+	
+	$('.toc').toc();
     }); 
 
 
